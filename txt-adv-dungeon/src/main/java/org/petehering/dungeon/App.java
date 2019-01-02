159a539c2d02e18640280ff7b986b0fc90762fcc
Pete Hering <pjhering@gmail.com>
2019-01-02 08:59:17 -0500

    add child projects

---
+++ txt-adv-dungeon/src/main/java/org/petehering/dungeon/App.java
@@ -0,0 +1,13 @@
+package org.petehering.dungeon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
